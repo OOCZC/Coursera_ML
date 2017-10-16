85ffaf95126e84eb61347d905ac336bd2d7be48e
OOC <whatisooc@gmail.com>
2017-10-16 17:51:41 +0800

    Update ml-ex6 to Chooce Parameters

--- ml-ex6/ex6/dataset3Params.m
+++ ml-ex6/ex6/dataset3Params.m
@@ -23,11 +23,24 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% train code by ooc
+%{ 
+par = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+for i = 1:8,
+for j = 1:8,
+model = svmTrain(X, y, par(i), @(x1, x2) gaussianKernel(x1, x2, par(j)));
+predictions = svmPredict(model, Xval);
 
+mean(double(predictions ~= yval))   %求平均值
+i,j
 
+end;
+end;
+%}
 
-
+C = 1;
+sigma = 0.1;
 
 % =========================================================================
 
